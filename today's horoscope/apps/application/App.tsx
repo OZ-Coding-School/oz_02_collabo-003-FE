f0e8d61ba39fb5a097d8100168fb3a91e4ae1ba8
Devinix00 <dpldpl123456@naver.com>
2024-04-19 15:48:48 +0900

    chore: gitignore 환경변수 파일 추가

--- today's horoscope/apps/application/App.tsx
+++ today's horoscope/apps/application/App.tsx
@@ -1,18 +1,11 @@
 import { StyleSheet, Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
-import { Button } from "@repo/ui";
 
 export default function Native() {
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Native</Text>
-      <Button
-        onClick={() => {
-          console.log("Pressed!");
-          alert("Pressed!");
-        }}
-        text="Boop"
-      />
+
       <StatusBar style="auto" />
     </View>
   );
