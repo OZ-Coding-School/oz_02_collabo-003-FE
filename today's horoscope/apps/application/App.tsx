0421d5de391c7de77888bb42c1b7f6e5e839181a
Devinix00 <dpldpl123456@naver.com>
2024-04-21 15:40:12 +0900

    chore: WebView 통신

--- today's horoscope/apps/application/App.tsx
+++ today's horoscope/apps/application/App.tsx
@@ -1,26 +1,5 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import WebView from 'react-native-webview';
 
 export default function Native() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.header}>Native</Text>
-
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <WebView source={{ uri: 'http://localhost:5173/' }} style={{ flex: 1 }} />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  header: {
-    fontWeight: 'bold',
-    marginBottom: 20,
-    fontSize: 36,
-  },
-});
