a242d2e528b35d2a788dc5296c24ea9b09fa0f91
Devinix00 <dpldpl123456@naver.com>
2024-04-22 16:15:52 +0900

    design: 사이드바 탭 색상 변경

--- today's horoscope/apps/admin/app/_components/sidebar/SidebarTab.tsx
+++ today's horoscope/apps/admin/app/_components/sidebar/SidebarTab.tsx
@@ -21,7 +21,7 @@ function SidebarTab({ content, href }: SidebarTabProps) {
         <React.Fragment>
           <button
             onClick={() => setIsOpenedFunction(!isOpenedFunction)}
-            className={`relative hover:bg-blue-500 ${isOpenedFunction ? 'bg-blue-500' : 'bg-blue-400'} transition-colors text-white py-[10px] rounded-md`}>
+            className={`relative hover:bg-blue-700 ${isOpenedFunction ? 'bg-blue-700' : 'bg-blue-400'} transition-colors text-white py-[10px] rounded-md`}>
             {content}
             <p className="absolute top-[50%] translate-y-[-50%] right-[12px] text-lg">{isOpenedFunction ? '-' : '+'}</p>
           </button>
@@ -30,13 +30,13 @@ function SidebarTab({ content, href }: SidebarTabProps) {
             <React.Fragment>
               <Link
                 href={routes.PUSH_MESSAGE}
-                className={`bg-blue-400 text-white hover:bg-blue-500 h-[30px] ${pathname === routes.PUSH_MESSAGE && 'bg-blue-500'} flex justify-center items-center rounded-md`}>
+                className={`bg-blue-400 text-white hover:bg-blue-700 h-[30px] ${pathname === routes.PUSH_MESSAGE && 'bg-blue-700'} flex justify-center items-center rounded-md`}>
                 푸시 메시지 관리
               </Link>
 
               <Link
                 href={routes.CONTENTS}
-                className={`bg-blue-400 text-white hover:bg-blue-500 h-[30px] ${pathname === routes.CONTENTS && 'bg-blue-500'} flex justify-center items-center rounded-md`}>
+                className={`bg-blue-400 text-white hover:bg-blue-700 h-[30px] ${pathname === routes.CONTENTS && 'bg-blue-700'} flex justify-center items-center rounded-md`}>
                 콘텐츠 관리
               </Link>
             </React.Fragment>
@@ -46,7 +46,7 @@ function SidebarTab({ content, href }: SidebarTabProps) {
         <Link
           onClick={() => setIsOpenedFunction(false)}
           href={href as string}
-          className={`bg-blue-400 hover:bg-blue-500 ${pathname === href && 'bg-blue-500'} transition-colors text-white text-center py-[10px] rounded-md`}>
+          className={`bg-blue-400 hover:bg-blue-700 ${pathname === href && 'bg-blue-700'} transition-colors text-white text-center py-[10px] rounded-md`}>
           {content}
         </Link>
       )}
