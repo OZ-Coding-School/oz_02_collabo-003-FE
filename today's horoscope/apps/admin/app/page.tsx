d8cf9bf5c8fa7368388f7f591f7dcac7a44bf547
Devinix00 <dpldpl123456@naver.com>
2024-04-22 10:48:59 +0900

    chore: tailwind 세팅

--- today's horoscope/apps/admin/app/page.tsx
+++ today's horoscope/apps/admin/app/page.tsx
@@ -1,14 +1,7 @@
-'use client';
-
-import { Button } from '@repo/ui';
-
-import styles from '../styles/index.module.css';
-
 export default function Web() {
   return (
-    <div className={styles.container}>
-      <h1>Web</h1>
-      <Button onClick={() => console.log('Pressed!')} text="Boop" />
-    </div>
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
   );
 }
