7ccaf9567bd3f1485c6c3b6485b8c4bfc46166aa
Devinix00 <dpldpl123456@naver.com>
2024-04-25 16:42:38 +0900

    design: 통계 페이지 디자인 수정

--- today's horoscope/apps/admin/app/(withSidebar)/statistics/page.tsx
+++ today's horoscope/apps/admin/app/(withSidebar)/statistics/page.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import MainContainer from '../../_components/mainContainer/MainContainer';
-import StatisticsLeftSection from './components/statisticsLeftSection/StatisticsLeftSection';
-import StatisticsRightSection from './components/statisticsRightSection/StatisticsRightSection';
+import ContentTitle from '../../_components/contentTitle/ContentTitle';
+import Link from 'next/link';
 
 function Statistics() {
   return (
     <MainContainer>
-      <div className="flex gap-[50px]">
-        <StatisticsLeftSection />
-        <StatisticsRightSection />
+      <ContentTitle title="통계" />
+      <div className="w-[500px] h-[100px] bg-gray-200 border-t-2 border-b-2 border-gray-400 px-4 flex justify-between items-center">
+        <p className="text-lg">파이어베이스 통계</p>
+        <Link
+          href=""
+          className="w-[100px] h-[40px] bg-blue-500 text-white text-lg flex justify-center items-center rounded-lg">
+          바로 가기
+        </Link>
       </div>
     </MainContainer>
   );
