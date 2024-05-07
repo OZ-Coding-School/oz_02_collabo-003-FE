8ecd2e244c0b51c46f0c562e75dd7f286ee7e14d
hayeonbae7 <baehayeon@naver.com>
2024-05-08 01:51:08 +0900

    feat: 기본 이미지 추가

--- today's horoscope/apps/react-web/src/pages/home/components/CarouselBanner/CarouselBanner.tsx
+++ today's horoscope/apps/react-web/src/pages/home/components/CarouselBanner/CarouselBanner.tsx
@@ -3,11 +3,12 @@ import styles from './CarouselBanner.module.scss';
 interface carouselContents {
   title: string;
   content: string;
+  imgitem: string;
 }
-function CarouselBanner({ title, content }: carouselContents) {
+function CarouselBanner({ title, content, imgitem }: carouselContents) {
   return (
     <div className={styles.carouselBanner}>
-      <div className={styles.carouselImage}>이미지</div>
+      <img src={`public/K철학관img/섬/img_island_${imgitem}_default.png`} className={styles.carouselImage} />
       <div className={styles.carouselContents}>
         <h1 className={styles.title}>{title}</h1>
         <div className={styles.content}>{content}</div>
