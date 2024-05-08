d7eaef11d27694e2e95e0230e496f29925bf0e2c
hayeonbae7 <139304636+hayeonbae7@users.noreply.github.com>
2024-05-09 01:44:46 +0900

    Revert "React web/공통컴포넌트"

--- today's horoscope/apps/react-web/src/components/infoForm/components/BirthModal/BirthModal.tsx
+++ today's horoscope/apps/react-web/src/components/infoForm/components/BirthModal/BirthModal.tsx
@@ -1,17 +1,12 @@
 import styles from '../modal.module.scss';
 
-interface BirthProps {
-  ClickBirthModal: () => void;
-}
-
-function BirthModal({ ClickBirthModal }: BirthProps) {
+function BirthModal() {
   return (
     <div className={styles.modal}>
       <div className={styles.modalMain}>
         <div className={styles.modalHeader}>생년월일</div>
-        <button className={styles.button} onClick={ClickBirthModal}>
-          적용하기
-        </button>
+
+        <button className={styles.button}>적용하기</button>
       </div>
     </div>
   );
--- today's horoscope/apps/react-web/src/components/infoForm/components/MbtiModal/MbtiModal.tsx
+++ today's horoscope/apps/react-web/src/components/infoForm/components/MbtiModal/MbtiModal.tsx
@@ -1,55 +1,27 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import styles from '../modal.module.scss';
-import React from 'react';
 import './MbtiSwiper.scss';
-import { useState } from 'react';
-import { UserData } from '../../InfoForm';
 
-interface MbtiProps {
-  ClickMbtiModal: () => void;
-  userData: UserData;
-  setUserData: React.Dispatch<React.SetStateAction<UserData>>;
-}
-
-const MBTIList = [
-  'MBTI모름',
-  'ISTJ',
-  'ISTP',
-  'ISFJ',
-  'ISFP',
-  'INTJ',
-  'INTP',
-  'INFJ',
-  'INFP',
-  'ESTJ',
-  'ESTP',
-  'ESFJ',
-  'ESFP',
-  'ENTJ',
-  'ENTP',
-  'ENFJ',
-  'ENFP',
-];
-
-function MbtiModal({ ClickMbtiModal, userData, setUserData }: MbtiProps) {
-  const [mbtiText, setMbtiText] = useState('');
-
-  function handleSwipe() {
-    const activeSlideContent = document.querySelector('.swiper-slide-active')?.textContent;
-
-    if (activeSlideContent) {
-      setMbtiText(activeSlideContent);
-    }
-    console.log(mbtiText);
-  }
-
-  function handleClick() {
-    setUserData({
-      ...userData,
-      mbti: mbtiText,
-    });
-    ClickMbtiModal();
-  }
+function BirthModal() {
+  const MBTIList = [
+    'MBTI모름',
+    'ISTJ',
+    'ISTP',
+    'ISFJ',
+    'ISFP',
+    'INTJ',
+    'INTP',
+    'INFJ',
+    'INFP',
+    'ESTJ',
+    'ESTP',
+    'ESFJ',
+    'ESFP',
+    'ENTJ',
+    'ENTP',
+    'ENFJ',
+    'ENFP',
+  ];
   return (
     <div className={styles.modal}>
       <div className={styles.modalMain}>
@@ -60,8 +32,7 @@ function MbtiModal({ ClickMbtiModal, userData, setUserData }: MbtiProps) {
             loop={true}
             direction="vertical"
             centeredSlides={true}
-            className={styles.swiperWrapper}
-            onSlideChange={handleSwipe}>
+            className={styles.swiperWrapper}>
             {MBTIList.map((content, index) => (
               <SwiperSlide key={index} className={styles.swiperSlide}>
                 {content}
@@ -69,12 +40,10 @@ function MbtiModal({ ClickMbtiModal, userData, setUserData }: MbtiProps) {
             ))}
           </Swiper>
         </div>
-        <button onClick={handleClick} className={styles.button}>
-          적용하기
-        </button>
+        <button className={styles.button}>적용하기</button>
       </div>
     </div>
   );
 }
 
-export default MbtiModal;
+export default BirthModal;
