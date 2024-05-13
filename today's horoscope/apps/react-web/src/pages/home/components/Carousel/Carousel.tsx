5bee5ec3d42c0816e0c03cdaa8e03d9d09910400
hayeonbae7 <baehayeon@naver.com>
2024-05-13 20:48:27 +0900

    feat: 상세페이지 라우팅 처리

--- today's horoscope/apps/react-web/src/pages/home/components/Carousel/Carousel.tsx
+++ today's horoscope/apps/react-web/src/pages/home/components/Carousel/Carousel.tsx
@@ -18,9 +18,18 @@ const imgList = ['today', 'zodiac', 'star', 'mbti'];
 function Carousel({ activeSlide, setActiveSlide }: swiperProps) {
   const navigate = useNavigate();
 
-  function MoveLogin() {
-    if (localStorage.length === 0) navigate('/login');
-    else navigate('/detail');
+  function MoveRoute(value: string) {
+    return () => {
+      if (localStorage.length === 0) {
+        navigate('/login');
+      } else if (value === 'mbti') {
+        navigate('/detail-mbti');
+      } else if (value === 'zodiac') {
+        navigate('/detail-zodiac/main');
+      } else if (value === 'star') {
+        navigate('/detail-star');
+      }
+    };
   }
 
   function handleSlwiper(swiper: swiper) {
@@ -53,7 +62,7 @@ function Carousel({ activeSlide, setActiveSlide }: swiperProps) {
           <SwiperSlide id={`slide-${imgList[index]}`} key={index} className="swiper-slide">
             <CarouselBanner imgitem={imgList[index]} title={SlideContent} content="content" />
             {imgList[index] === 'today' ? null : (
-              <button onClick={MoveLogin} className="contentsDetail">
+              <button onClick={MoveRoute(imgList[index])} className="contentsDetail">
                 운세
                 <br />
                 더보기
