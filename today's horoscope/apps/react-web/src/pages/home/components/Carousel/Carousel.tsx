c69b8d11c8451c44cf5a85afa3d541d2bec5132a
hayeonbae7 <baehayeon@naver.com>
2024-05-14 02:22:00 +0900

    design: 더보기버튼에 transition 추가

--- today's horoscope/apps/react-web/src/pages/home/components/Carousel/Carousel.tsx
+++ today's horoscope/apps/react-web/src/pages/home/components/Carousel/Carousel.tsx
@@ -61,15 +61,15 @@ function Carousel({ activeSlide, setActiveSlide }: swiperProps) {
         {Slides.map((SlideContent, index) => (
           <SwiperSlide id={`slide-${imgList[index]}`} key={index} className="swiper-slide">
             <CarouselBanner imgitem={imgList[index]} title={SlideContent} content="content" />
-            {imgList[index] === 'today' ? null : (
-              <button onClick={MoveRoute(imgList[index])} className="contentsDetail">
-                운세
-                <br />
-                더보기
-                <br />
-                <MdKeyboardArrowDown size={30} />
-              </button>
-            )}
+            <button
+              onClick={MoveRoute(imgList[index])}
+              className={imgList[index] === 'today' ? 'contentsDetail' : 'contentsDetail activeContentDetail'}>
+              운세
+              <br />
+              더보기
+              <br />
+              <MdKeyboardArrowDown size={30} />
+            </button>
           </SwiperSlide>
         ))}
       </Swiper>
