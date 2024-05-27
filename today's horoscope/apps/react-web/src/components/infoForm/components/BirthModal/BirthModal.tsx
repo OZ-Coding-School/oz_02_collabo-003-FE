bc613c838b8a4b1d41cee2411458f887067d0464
hayeonbae7 <baehayeon@naver.com>
2024-05-27 11:40:31 +0900

    feat: 유저 생일 정보 초기값으로 설정

--- today's horoscope/apps/react-web/src/components/infoForm/components/BirthModal/BirthModal.tsx
+++ today's horoscope/apps/react-web/src/components/infoForm/components/BirthModal/BirthModal.tsx
@@ -20,6 +20,10 @@ function BirthModal({ clickBirthModal, userData, setUserData }: BirthProps) {
   const [activeYear, setActiveYear] = useState<string>('2007');
   const [activeMonth, setActiveMonth] = useState<string>('1');
   const [activeDay, setActiveDay] = useState<string>('1');
+  const [userBirth, setUserBirth] = useState<string>('');
+  const [userYear, setUserYear] = useState<number>(0);
+  const [userMonth, setUserMonth] = useState<number>(0);
+  const [userDay, setUserDay] = useState<number>(0);
 
   function handleSwiper(swiper: swiper) {
     const activeSlide = swiper.slides[swiper.activeIndex];
@@ -50,9 +54,31 @@ function BirthModal({ clickBirthModal, userData, setUserData }: BirthProps) {
   }
 
   useEffect(() => {
+    // 유저 정보로 스와이퍼 초기값 변경
+    const storedData = localStorage.getItem('userData');
+    if (storedData) {
+      const objectStoredData = JSON.parse(storedData);
+      setUserBirth(objectStoredData.birth);
+      const userYearString = userBirth.split('-')[0];
+      const userYear = parseInt(userYearString);
+      const yearIndex = yearList.indexOf(userYear);
+      setUserYear(yearIndex >= 0 ? yearIndex : 0);
+      const userMonthString = userBirth.split('-')[1];
+      const userMonth = parseInt(userMonthString);
+      const monthIndex = monthList.indexOf(userMonth);
+      setUserMonth(monthIndex >= 0 ? monthIndex : 0);
+      const userDayString = userBirth.split('-')[2];
+      const userDay = parseInt(userDayString);
+      const dayIndex = dayList.indexOf(userDay);
+      setUserDay(dayIndex >= 0 ? dayIndex : 0);
+    }
     const newBirthText = `${activeYear}-${activeMonth}-${activeDay}`;
     setBirthText(newBirthText);
-  }, [activeYear, activeMonth, activeDay]);
+  }, [userBirth, activeYear, activeMonth, activeDay]);
+
+  console.log(userYear);
+  console.log(userMonth);
+  console.log(userDay);
 
   return (
     <div>
@@ -68,9 +94,11 @@ function BirthModal({ clickBirthModal, userData, setUserData }: BirthProps) {
             speed={500}
             className="swiper-wrapper"
             id="yearSwiper"
+            initialSlide={userYear}
+            key={`yearSwiper-${userYear}`}
             onSlideChange={handleSwiper}>
             {yearList.map((content, index) => (
-              <SwiperSlide key={index} className="swiper-slide">
+              <SwiperSlide key={`year-${index}`} className="swiper-slide">
                 {content}년
               </SwiperSlide>
             ))}
@@ -82,9 +110,11 @@ function BirthModal({ clickBirthModal, userData, setUserData }: BirthProps) {
             speed={500}
             className="swiper-wrapper"
             id="monthSwiper"
+            initialSlide={userMonth}
+            key={`monthSwiper-${userMonth}`}
             onSlideChange={handleSwiper}>
             {monthList.map((content, index) => (
-              <SwiperSlide key={index} className="swiper-slide">
+              <SwiperSlide key={`month-${index}`} className="swiper-slide">
                 {content}월
               </SwiperSlide>
             ))}
@@ -96,9 +126,11 @@ function BirthModal({ clickBirthModal, userData, setUserData }: BirthProps) {
             speed={500}
             className="swiper-wrapper"
             id="daySwiper"
+            initialSlide={userDay}
+            key={`daySwiper-${userDay}`}
             onSlideChange={handleSwiper}>
             {dayList.map((content, index) => (
-              <SwiperSlide key={index} className="swiper-slide">
+              <SwiperSlide key={`day-${index}`} className="swiper-slide">
                 {content}일
               </SwiperSlide>
             ))}
