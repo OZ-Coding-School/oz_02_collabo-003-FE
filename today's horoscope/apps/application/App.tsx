dd83a97839768132d628297830601e47fd07b27a
Devinix00 <dpldpl123456@naver.com>
2024-05-30 20:54:01 +0900

    feat: 뒤로가기로 어플 종료 기능 구현 (토스트)

--- today's horoscope/apps/application/App.tsx
+++ today's horoscope/apps/application/App.tsx
@@ -1,34 +1,41 @@
-import React, { useEffect, useRef } from 'react';
-import { BackHandler, Platform, View } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { BackHandler, Platform, View, ToastAndroid } from 'react-native';
 import WebView from 'react-native-webview';
 import * as SplashScreen from 'expo-splash-screen';
 
 export default function Native() {
   const webViewRef = useRef<WebView>(null);
+  const [isCanGoBack, setIsCanGoBack] = useState(false);
+  const [lastBackPressed, setLastBackPressed] = useState(0);
 
   SplashScreen.preventAutoHideAsync();
 
-  const onAndroidBackPress = () => {
-    if (webViewRef.current) {
+  const onPressHardwareBackButton = () => {
+    const now = Date.now();
+    if (webViewRef.current && isCanGoBack) {
       webViewRef.current.goBack();
       return true;
+    } else if (now - lastBackPressed <= 2000) {
+      BackHandler.exitApp();
+      return true;
+    } else {
+      ToastAndroid.show('뒤로가기 버튼을 한번 더 누르면 종료됩니다.', ToastAndroid.SHORT);
+      setLastBackPressed(now);
+      return true;
     }
-    return false;
   };
 
   useEffect(() => {
-    if (Platform.OS === 'android') {
-      BackHandler.addEventListener('hardwareBackPress', onAndroidBackPress);
-      return () => {
-        BackHandler.removeEventListener('hardwareBackPress', onAndroidBackPress);
-      };
-    }
-  }, []);
+    BackHandler.addEventListener('hardwareBackPress', onPressHardwareBackButton);
+    return () => {
+      BackHandler.removeEventListener('hardwareBackPress', onPressHardwareBackButton);
+    };
+  }, [isCanGoBack, lastBackPressed]);
 
   const handleLoad = () => {
     setTimeout(() => {
       SplashScreen.hideAsync();
-    }, 850);
+    }, 1000);
   };
 
   return Platform.OS === 'web' ? (
@@ -43,6 +50,30 @@ export default function Native() {
         allowsBackForwardNavigationGestures={true}
         source={{ uri: 'https://today-s-horoscope.vercel.app/' }}
         onLoadEnd={handleLoad}
+        injectedJavaScript={`
+          (function() {
+            function wrap(fn) {
+              return function wrapper() {
+                var res = fn.apply(this, arguments);
+                window.ReactNativeWebView.postMessage('navigationStateChange');
+                return res;
+              }
+            }
+      
+            history.pushState = wrap(history.pushState);
+            history.replaceInstance = wrap(history.replaceState);
+            window.addEventListener('popstate', function() {
+              window.ReactNativeWebView.postMessage('navigationStateChange');
+            });
+          })();
+      
+          true;
+        `}
+        onMessage={({ nativeEvent: state }) => {
+          if (state.data === 'navigationStateChange') {
+            setIsCanGoBack(state.canGoBack);
+          }
+        }}
       />
     </View>
   );
