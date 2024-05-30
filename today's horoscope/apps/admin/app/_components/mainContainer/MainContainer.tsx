4cdc36419f2eaaa85688c6062907494f9f6df21c
Devinix00 <dpldpl123456@naver.com>
2024-05-31 02:03:28 +0900

    remove: admin 삭제

--- today's horoscope/apps/admin/app/_components/mainContainer/MainContainer.tsx
+++
@@ -1,7 +0,0 @@
-import React, { PropsWithChildren } from 'react';
-
-function MainContainer({ children }: PropsWithChildren) {
-  return <div className="w-full p-10 border-t-[2px] border-l-[2px] border-gray-100">{children}</div>;
-}
-
-export default MainContainer;
--- today's horoscope/apps/admin/app/_images/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Logo } from './logo.png';
-export { default as Dropdown } from './dropdown.png';
-export { default as PrevArrow } from './prevArrow.png';
-export { default as NextArrow } from './nextArrow.png';
