74673165b17fcbf31012fdcba0ad6edd842ca209
서니 <62998244+Sprisun12@users.noreply.github.com>
2024-06-03 12:12:00 +0900

    design: 업데이트 버튼 삭제

--- today's horoscope/apps/react-web/src/pages/version/Version.tsx
+++ today's horoscope/apps/react-web/src/pages/version/Version.tsx
@@ -4,38 +4,19 @@ import { useNavigate } from 'react-router-dom';
 import Styles from './Version.module.scss';
 
 const Version: React.FC = () => {
-  const [appVersion, setAppVersion] = useState<string>('2.1.5');
-  const [showUpdatePopup, setShowUpdatePopup] = useState<boolean>(false);
+  // 현재 앱 버전 상태
+  const [appVersion] = useState<string>('2.1.5');
+  // 푸시 알림 설정 상태
   const [pushNotification, setPushNotification] = useState<boolean>(false);
-
+  // 푸시 알림 설정 토글 함수
   const togglePushNotification = () => {
     setPushNotification(prevState => !prevState);
   };
 
-  const updateAppVersion = (newVersion: string) => {
-    setAppVersion(newVersion);
-  };
-
-  const handleVersionClick = () => {
-    updateAppVersion('');
-    setShowUpdatePopup(true);
-  };
-
-  const handlePushNotificationChange = () => {
-    togglePushNotification();
-  };
-
+  // 페이지 이동 네비게이트 훅
   const navigate = useNavigate();
   const moveHome = () => {
     navigate('/');
-  };
-
-  const handleUpdateConfirm = () => {
-    setShowUpdatePopup(false);
-  };
-
-  const handleUpdateCancel = () => {
-    setShowUpdatePopup(false);
   };
 
   return (
@@ -50,46 +31,23 @@ const Version: React.FC = () => {
           <h1 className={Styles.textTitle}>버전 관리</h1>
           <span className={Styles.textSub}>현재 버전 {appVersion}</span>
         </div>
-        <button className={Styles.updateButton} onClick={handleVersionClick}>
-          업데이트
-        </button>
       </div>
       <div className={Styles.pushContents}>
         <label className={Styles.switch}>
-          <h1 className={Styles.textTitle}>푸시알람설정</h1>
-          <span className={Styles.textSub}> 푸시알람을 ON/OFF 하실 수 있습니다.</span>
+          <h1 className={Styles.textTitle}>푸시 알림 설정</h1>
+          <span className={Styles.textSub}>푸시 알림을 켜거나 끌 수 있습니다.</span>
         </label>
         <div className={Styles.wrapper}>
           <input
             type="checkbox"
             id="switch"
             checked={pushNotification}
-            onChange={handlePushNotificationChange}
+            onChange={togglePushNotification}
             className={Styles.toggleSwitch}
           />
           <label htmlFor="switch" className={Styles.onfBtn}></label>
         </div>
       </div>
-
-      {showUpdatePopup && (
-        <>
-          <div className={Styles.popupBackground}></div>
-          <div className={Styles.popup}>
-            <div>
-              <h2 className={Styles.popupTitle}>새로운 버전이 있습니다</h2>
-              <p className={Styles.popupText}>최신버전으로 업데이트를 위해 스토어로 이동하시겠습니까?</p>
-            </div>
-            <div className={Styles.buttonContainer}>
-              <button className={Styles.Ok} onClick={handleUpdateConfirm}>
-                확인
-              </button>
-              <button className={Styles.No} onClick={handleUpdateCancel}>
-                취소
-              </button>
-            </div>
-          </div>
-        </>
-      )}
     </div>
   );
 };
