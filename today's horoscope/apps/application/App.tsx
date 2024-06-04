1a9339748f3e3632a97610d71e3a460fdae3850f
Devinix00 <dpldpl123456@naver.com>
2024-06-04 13:04:22 +0900

    feat: 인터넷 연결 안되었을때 ui 처리

--- today's horoscope/apps/application/App.tsx
+++ today's horoscope/apps/application/App.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { BackHandler, View, ToastAndroid } from 'react-native';
+import { BackHandler, View, ToastAndroid, Text } from 'react-native';
 import WebView from 'react-native-webview';
 import * as SplashScreen from 'expo-splash-screen';
+import NetInfo from '@react-native-community/netinfo';
 
 type WebViewNavigation = {
   url: string;
@@ -15,6 +16,14 @@ export default function Native() {
   const webViewRef = useRef<WebView>(null);
   const [currentUrl, setCurrentUrl] = useState('');
   const [lastBackPressed, setLastBackPressed] = useState(0);
+  const [isConnected, setIsConnected] = useState<boolean | null>(true);
+
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener(state => {
+      setIsConnected(state.isConnected);
+    });
+    return () => unsubscribe();
+  }, []);
 
   SplashScreen.preventAutoHideAsync();
 
@@ -54,13 +63,37 @@ export default function Native() {
 
   return (
     <View style={{ flex: 1 }}>
-      <WebView
-        style={{ flex: 1 }}
-        source={{ uri: 'https://today-s-horoscope.vercel.app/' }}
-        onNavigationStateChange={handleNavigationStateChange}
-        ref={webViewRef}
-        onLoadEnd={handleLoad}
-      />
+      {isConnected ? (
+        <WebView
+          style={{ flex: 1 }}
+          source={{ uri: 'https://today-s-horoscope.vercel.app/' }}
+          onNavigationStateChange={handleNavigationStateChange}
+          ref={webViewRef}
+          onLoadEnd={handleLoad}
+        />
+      ) : (
+        <View
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '100%',
+            height: '100%',
+            backgroundColor: '#191a18',
+          }}>
+          <View
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              gap: 24,
+            }}>
+            <Text style={{ fontSize: 18, fontWeight: '500', color: '#d3d3d3' }}>인터넷 연결</Text>
+            <Text style={{ fontSize: 14, fontWeight: '500', color: '#838f90' }}>
+              오프라인 상태입니다. 인터넷 연결을 확인하세요.
+            </Text>
+          </View>
+        </View>
+      )}
     </View>
   );
 }
