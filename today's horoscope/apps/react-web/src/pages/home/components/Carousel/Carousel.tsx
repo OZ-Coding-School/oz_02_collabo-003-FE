168a590829b6ad64501bfc2e3dc65fce7441a4ff
hayeonbae7 <baehayeon@naver.com>
2024-06-12 21:51:30 +0900

    fest: 메인배너 드래그 이벤트로 수정

--- today's horoscope/apps/react-web/src/pages/home/components/Carousel/Carousel.tsx
+++ today's horoscope/apps/react-web/src/pages/home/components/Carousel/Carousel.tsx
@@ -2,7 +2,8 @@ import CarouselBanner from '../CarouselBanner/CarouselBanner';
 import './Csrousel.scss';
 import { useNavigate } from 'react-router-dom';
 import { IoIosArrowDown } from 'react-icons/io';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { useDrag } from '@use-gesture/react';
 
 interface swiperProps {
   setActiveSlide: React.Dispatch<React.SetStateAction<string>>;
@@ -17,6 +18,8 @@ function Carousel({ setActiveSlide }: swiperProps) {
   const [imgValue, setImgValue] = useState<string[]>(imgList);
   const [userValue, setUserValue] = useState<string[]>(user);
   const [selectedItem, setSelectedItem] = useState<number>(0);
+
+  const containerRef = useRef<HTMLDivElement>(null);
 
   const navigate = useNavigate();
 
@@ -64,54 +67,61 @@ function Carousel({ setActiveSlide }: swiperProps) {
     }
   }, [setActiveSlide]);
 
-  //현재 배너의 인덱스 저장하고 배경색 변경을 위한 배너값 저장
-  const handleClickBanner = (index: number) => {
-    setSelectedItem(index);
-    setActiveSlide(imgValue[index]);
-  };
+  // 현재 배너의 인덱스 저장하고 배경색 변경을 위한 배너값 저장
+  const bind = useDrag(
+    ({ active, movement: [mx], direction: [xDir], cancel }) => {
+      if (active && Math.abs(mx) > containerRef.current!.clientWidth / 2) {
+        setSelectedItem(prev => {
+          const newIndex = (prev + (xDir > 0 ? -1 : 1) + Slides.length) % Slides.length;
+          if (newIndex < 0) setActiveSlide(imgValue[3]);
+          else setActiveSlide(imgValue[newIndex]);
+          return newIndex;
+        });
+        cancel();
+      }
+    },
+    { filterTaps: true },
+  );
 
   return (
     <div className="slider-container">
-      <ul className="slider-items">
-        {slidesValue.map((SlideContent, index) => {
-          let offset = index - selectedItem;
-          if (offset < 0) offset += Slides.length;
-          console.log(imgValue[index]);
-          return (
-            <li
-              id={`slide-${imgValue[index]}`}
-              key={index}
-              onClick={() => handleClickBanner(index)}
-              className={`item-${offset + 1}`}>
-              <CarouselBanner imgitem={imgValue[index]} user={userValue[index]} title={SlideContent} />
-              <button
-                onClick={moveDetail(imgValue[index])}
-                className={
-                  localStorage.userData !== undefined && imgValue[index] === 'today'
-                    ? 'contentsDetail'
-                    : 'contentsDetail activeContentDetail'
-                }>
-                {localStorage.userData === undefined ? (
-                  <div>
-                    오늘의 운세
-                    <br />
-                    더보기
-                    <br />
-                  </div>
-                ) : (
-                  <div>
-                    운세
-                    <br />
-                    더보기
-                    <br />
-                  </div>
-                )}
-                <IoIosArrowDown className="detailIcon" size={30} />
-              </button>
-            </li>
-          );
-        })}
-      </ul>
+      <div className="carousel-slider" {...bind()} ref={containerRef} style={{ touchAction: 'none' }}>
+        <ul className="slider-items">
+          {slidesValue.map((SlideContent, index) => {
+            let offset = index - selectedItem;
+            if (offset < 0) offset += Slides.length;
+            return (
+              <li id={`slide-${imgValue[index]}`} key={index} className={`item-${offset + 1}`}>
+                <CarouselBanner imgitem={imgValue[index]} user={userValue[index]} title={SlideContent} />
+                <button
+                  onClick={moveDetail(imgValue[index])}
+                  className={
+                    localStorage.userData !== undefined && imgValue[index] === 'today'
+                      ? 'contentsDetail'
+                      : 'contentsDetail activeContentDetail'
+                  }>
+                  {localStorage.userData === undefined ? (
+                    <div>
+                      오늘의 운세
+                      <br />
+                      더보기
+                      <br />
+                    </div>
+                  ) : (
+                    <div>
+                      운세
+                      <br />
+                      더보기
+                      <br />
+                    </div>
+                  )}
+                  <IoIosArrowDown className="detailIcon" size={30} />
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
